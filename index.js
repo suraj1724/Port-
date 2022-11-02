2071b809c96aaf82124179ba98d4787d285af0ba
suraj1724 <113453491+suraj1724@users.noreply.github.com>
2022-11-02 21:24:39 +0530

    project port
    new

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("suraj");
+setInterval(bodyload,1000)
+function bodyload(){
+    var Dat=new Date()
+    var d = document.getElementById("date");
+  
+   c=  Dat.toLocaleTimeString();
+   e= Dat.toLocaleString()
+   f=
+   d.innerHTML=c;
+   d.innerHTML=e;
+}
